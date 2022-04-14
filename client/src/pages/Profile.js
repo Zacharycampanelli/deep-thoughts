fbb3d97e27fe1af72657002e92fc6b83549da8b7
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2022-04-14 15:03:11 -0400

    user can add friends and thought form is available

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -2,21 +2,35 @@ import React from 'react';
 import { Redirect, useParams } from 'react-router-dom';
 
 import ThoughtList from '../components/ThoughtList';
+import ThoughtForm from '../components/ThoughtForm';
 import FriendList from '../components/FriendList';
 
 import Auth from '../utils/auth';
-
-import { useQuery } from '@apollo/client';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import { ADD_FRIEND } from '../utils/mutations';
+
+import { useQuery, useMutation } from '@apollo/client';
 
 const Profile = () => {
   const { username: userParam } = useParams();
 
-  const { loading, data } = useQuery( userParam ? QUERY_USER : QUERY_ME,  {
+  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
     variables: { username: userParam },
   });
 
   const user = data?.me || data?.user || {};
+
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: { id: user._id },
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const [addFriend] = useMutation(ADD_FRIEND);
 
   if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
     return <Redirect to="/profile" />;
@@ -26,18 +40,21 @@ const Profile = () => {
     return <div>Loading...</div>;
   }
 
-  if(!user?.username) {
-    return (
-      <h4>
-        You need to be logged in to see this page. Use the navigation links above to sign up or log in!
-      </h4>
-    )
+  if (!user?.username) {
+    return <h4>You need to be logged in to see this page. Use the navigation links above to sign up or log in!</h4>;
   }
 
   return (
     <div>
       <div className="flex-row mb-3">
-        <h2 className="bg-dark text-secondary p-3 display-inline-block">Viewing {userParam ? `${user.username}'s` : 'your'} profile.</h2>
+        <h2 className="bg-dark text-secondary p-3 display-inline-block">
+          Viewing {userParam ? `${user.username}'s` : 'your'} profile.
+        </h2>
+        {userParam && (
+          <button className="btn ml-auto" onClick={handleClick}>
+            Add Friend
+          </button>
+        )}
       </div>
 
       <div className="flex-row justify-space-between mb-3">
@@ -47,6 +64,9 @@ const Profile = () => {
         <div className="col-12 col-lg-3 mb-3">
           <FriendList username={user.username} friendCount={user.friendCount} friends={user.friends} />
         </div>
+      </div>
+      <div className="mb-3">
+        {!userParam && <ThoughtForm />}
       </div>
     </div>
   );
